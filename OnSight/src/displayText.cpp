e8721f97f54acff2fa3fa82e2e79a6b01ea6ee89
PatrickPLi <lipatrick.1023@gmail.com>
2019-11-23 20:43:33 -0500

    Create displayText.cpp

---
+++ OnSight/src/displayText.cpp
@@ -0,0 +1,138 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Arduino.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     LED_BUILTIN // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  Serial.begin(115200);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  // Draw a single pixel in white
+  display.drawPixel(10, 10, SSD1306_WHITE);
+
+  // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+  delay(2000);
+  // display.display() is NOT necessary after every single drawing command,
+  // unless that's what you want...rather, you can batch up a bunch of
+  // drawing operations and then update the screen all at once by calling
+  // display.display(). These examples demonstrate both approaches...
+  
+  testdrawchar();      // Draw characters of the default font
+
+  testdrawstyles();     //Draw styles
+
+  testscrolltext();    // Draw scrolling text
+}
+
+void displayMessage(struct Data* data) {
+    String details = data->details;
+    int emergency_level = data->emergency_level;
+    String location = data->location;
+    String situation = data->situation;
+
+    if (details.length() <= 15) {
+        drawchar(details);
+    }
+    else {
+        drawscroll(details);
+    }
+
+    delay(5000);
+
+    String emergency = "Emergency Level: " + emergency_level;
+    drawchar(emergency);
+
+    delay(5000);
+
+    if (location.length() <= 15) {
+        drawchar(location);
+    }
+    else {
+        drawscroll(location);
+    }
+
+    delay(5000);
+
+    if (situation.length() <= 15) {
+        drawchar(situation);
+    }
+    else {
+        drawscroll(situation);
+    }
+}
+
+
+void drawchar(String s) {
+  display.clearDisplay();
+
+  display.setTextSize(3);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+
+  for (int i = 0; i < s.size(); i++) {
+      display.write(s[i]);
+  }
+  display.display();
+}
+
+void drawstyles(String s) {
+  display.clearDisplay();
+
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(F(s));
+  display.display();
+  delay(2000);
+}
+
+void drawscroll(String s) {
+  display.clearDisplay();
+
+  display.setTextSize(2); // Draw 2X-scale text
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(10, 0);
+  display.println(F(s));
+  display.display();      // Show initial text
+  delay(100);
+
+  // Scroll in various directions, pausing in-between:
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+  display.startscrollleft(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+  display.startscrolldiagright(0x00, 0x07);
+  delay(2000);
+  display.startscrolldiagleft(0x00, 0x07);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+}
