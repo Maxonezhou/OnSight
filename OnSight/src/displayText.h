b198c4b8b7e5017eceff68ff0b12ebfcef71fcb9
PatrickPLi <lipatrick.1023@gmail.com>
2019-11-23 20:45:46 -0500

    Update displayText.h

--- OnSight/src/displayText.h
+++ OnSight/src/displayText.h
@@ -11,42 +11,6 @@
 #define OLED_RESET     LED_BUILTIN // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-void setup() {
-  Serial.begin(115200);
-
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  display.display();
-  delay(2000); // Pause for 2 seconds
-
-  // Clear the buffer
-  display.clearDisplay();
-
-  // Draw a single pixel in white
-  display.drawPixel(10, 10, SSD1306_WHITE);
-
-  // Show the display buffer on the screen. You MUST call display() after
-  // drawing commands to make them visible on screen!
-  display.display();
-  delay(2000);
-  // display.display() is NOT necessary after every single drawing command,
-  // unless that's what you want...rather, you can batch up a bunch of
-  // drawing operations and then update the screen all at once by calling
-  // display.display(). These examples demonstrate both approaches...
-  
-  testdrawchar();      // Draw characters of the default font
-
-  testdrawstyles();     //Draw styles
-
-  testscrolltext();    // Draw scrolling text
-}
-
 void displayMessage(struct Data* data) {
     String details = data->details;
     int emergency_level = data->emergency_level;
