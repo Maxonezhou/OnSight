f4c734e1e1f74659f91f9ce6fbaa6a10eff1c4ec
Max Zhou <maxonezhou@gmail.com>
2019-11-23 20:13:54 -0500

    commit

---
+++ OnSight/include/Credentials.h
@@ -0,0 +1,7 @@
+#define SSID "HackWestern6"
+#define PASSWORD "42259378376"
+
+#define FIREBASE_HOST "onsight-88888.firebaseio.com"
+#define FIREBASE_AUTH "EVixsuIwzMaTEtOcECwnvSoTiI0Mb7NcRBCM7Hjs"
+
+#define GOOGLE_API_KEY "AIzaSyDh31XdRlMxyCSInWBoZv5vyY14ChG4EQk"
